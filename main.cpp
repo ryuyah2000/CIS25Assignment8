ac26cc1b65de2069f4b3f95bd3226efab9fb92dd
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-09-17 14:32:04 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// code by Ryuya Hirota
+
+#include <iostream>
+
+int main() {
+    // how can we change the code so the result of the division is a double value?
+    int x = 10;
+    int y = 3;
+    double z = x / y;
+    
+    std::cout << z << std::endl;
+    
+    
+    // change one of the values to be double
+    double x2 = 10;
+    int y2 = 3;
+    double z2 = x2 / y2;
+    
+    std::cout << z2 << std::endl;
+    return 0;
+}
